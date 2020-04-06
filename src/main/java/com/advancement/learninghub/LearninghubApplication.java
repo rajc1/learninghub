98ef6b5a459066e4764dca517ccbbab8aa688efa
rajc1 <chetan.here@outlook.com>
2020-04-06 19:02:02 +0530

    spring boot first commit

---
+++ src/main/java/com/advancement/learninghub/LearninghubApplication.java
@@ -0,0 +1,15 @@
+package com.advancement.learninghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearninghubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearninghubApplication.class, args);
+    }
+
+
+
+}
